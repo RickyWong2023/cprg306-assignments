e328099aaa85cfdba9c5a9ea4b047bf40534233e
RickyWong2023 <151792764+RickyWong2023@users.noreply.github.com>
2024-09-19 13:11:18 -0600

    week2

---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function StudentInfo(){
+
+  return(
+    <main>
+      <h1>Wong Ka Tsun</h1>
+      <Link href="https://github.com/RickyWong2023">My GitHub</Link>
+    </main>
+  )
+}
