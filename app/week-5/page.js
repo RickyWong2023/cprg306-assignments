5eb6f1d7ad39caacc042aa09c6966a62ec16a5a2
RickyWong2023 <151792764+RickyWong2023@users.noreply.github.com>
2024-10-14 21:52:40 -0600

    week-5

---
+++ app/week-5/page.js
@@ -0,0 +1,11 @@
+
+
+import NewItem from './new-item';
+
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+      <NewItem />
+    </div>
+  );
+}
