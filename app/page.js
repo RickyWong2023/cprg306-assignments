d3aac36d754cd91dcf238c6efbe43595d76dd035
RickyWong2023 <tsunswc2012@gmail.com>
2024-12-07 23:30:56 -0700

    first commit

--- app/page.js
+++ app/page.js
@@ -1,20 +1,22 @@
-import Link from "next/link"; 
+"use client"
+import React from "react";
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <main>
-      <h1>CPRG 306: Web Development 2 - Assignments</h1>
-      <ul>
-        <li><Link href="./week-2/">Week 2 Assignment</Link></li>
-        <li><Link href="./week-3/">Week 3 Assignment</Link></li>
-        <li><Link href="./week-4/">Week 4 Assignment</Link></li>
-        <li><Link href="./week-5/">Week 5 Assignment</Link></li>
-        <li><Link href="./week-6/">Week 6 Assignment</Link></li>
-        <li><Link href="./week-7/">Week 7 Assignment</Link></li>
-        <li><Link href="./week-8/">Week 8 Assignment</Link></li>
-        <li><Link href="./week-9/">Week 9 Assignment</Link></li>
-        <li><Link href="./week-10/">Week 10 Assignment</Link></li>
-      </ul>
+    <main className="w-full h-screen" style={{ 
+      backgroundImage: "url('https://images8.alphacoders.com/128/1289648.jpg')", 
+      backgroundSize: "cover", 
+      backgroundPosition: "center", 
+      backgroundRepeat: "no-repeat" 
+    }}>
+      <div className="ml-4 mb-12">
+        <h1 className="text-5xl font-bold mb-4 text-black">League of Legends Guide</h1>
+        <p className="text-xl text-gray-800">Your comprehensive resource for mastering the game</p>
+      </div>
+
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
+      </div>
     </main>
   );
 }
